b8d157d5c7a8304e8b06a9f2f60bdb342fab0ae0
razorblade23 <josip.vivo.cubric@gmail.com>
2025-06-11 16:59:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import cowsay
+
+message = """
+The most remarkable thing about my mother is that for thirty years she served
+the family nothing but leftovers.  The original meal has never been found.
+		-- Calvin Trillin
+""".strip()
+
+print(cowsay.get_output_string("cow", message))
